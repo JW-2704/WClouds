2b0efe5b669ea9e52a27e98a8b00df4e439e0a5f
karan <karanozcelik09@gmail.com>
2026-06-02 19:23:32 +0200

    letzter commit fürs backend weil neues repo

--- Backend/routers/auth.py
+++ Backend/routers/auth.py
@@ -1,17 +1,23 @@
 from fastapi import Security, HTTPException
 from fastapi.security import APIKeyHeader
+import secrets
 # Gibt an, dass wir im HTTP-Header nach einem Feld namens "X-API-Key" suchen
 
 
 api_key_header = APIKeyHeader(name="X-API-Key")
 
 
-api_keys = []
-def create_api_key():
-    pass
+api_keys = {}
+def create_api_key(user_id):
+    new_key = secrets.token_urlsafe(32)
+    api_keys.update({user_id:new_key})
+    return new_key
 
-def get_api_key(user_id: int):
-    pass
+def delete_api_key(user_id):
+    api_keys.pop(user_id)
+
+def get_api_key(user_id: int) -> str:
+    return api_keys.get(user_id)
 
 # Prüft das Passwort. Stimmt es nicht → HTTP 401 (Unauthorized)
 def verify_api_key(sent_api_key: str = Security(api_key_header)):
